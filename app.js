daaf36a33531bc07314780fd41d07df2ef07bd84
Mayur Kapadi <125800989+mkkapadi12@users.noreply.github.com>
2024-12-09 23:41:18 +0530

    change cors origin

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const ConnectDB = require("./DB/connect");
 //cors
 app.use(
   cors({
-    origin: "http://localhost:5173/",
-    // methods: ["GET", "POST"],
+    origin: "*",
+    methods: ["GET", "POST"],
   })
 );
 
